c52998a1b04678edc0fe2689392a22538d0563b1
Dawoud17 <96065576+Dawoud17@users.noreply.github.com>
2021-12-13 06:31:53 -0800

    commit

---
+++ src/components/Chiangmai.vue
@@ -0,0 +1,39 @@
+<template>
+    <h1>Chiang Mai</h1>
+
+<div class="pics">
+  <img src="../assets/Chiang_Mai/floating.jpg">
+  <img src="../assets/Chiang_Mai/lantern.jpg">
+</div>
+<div class="pics">  
+    <img src="../assets/Chiang_Mai/stars.jpg">
+    <img src="../assets/Chiang_Mai/sticky.jpg">
+</div>
+<div class="pics">
+    <img src="../assets/Chiang_Mai/suthep.jpg">
+    <img src="../assets/Chiang_Mai/temple.jpg">
+</div>
+<div class="pics">
+    <img src="../assets/Chiang_Mai/elephant.jpg">
+     <img src="../assets/Chiang_Mai/view.jpg">
+</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.h1{
+    font-size: 35pt;
+    
+}
+.pics{
+  height: 800px;
+  width: 500px;
+  display: flex;
+  align-content: center;
+}
+</style>
